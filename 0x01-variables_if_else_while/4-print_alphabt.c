c0be29202dc52a31edfd6013dd7e29837b3bc76d
Abassgold <abasskola10@gmail.com>
2022-09-11 11:31:10 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+/**
+ * main- entry point
+ * Description: Alphabets
+ * Return: zero on success
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'e')
+		{
+			etter++;
+		}
+		else if (letter == 'q')
+		{
+			letter++;
+
+		}
+		else
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
